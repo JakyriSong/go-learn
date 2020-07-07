39baecc4e38de0db2da950931776d81a2818dbdd
songjiajun <songjiajun@baidu.com>
2020-07-07 22:26:14 +0800

    fourth day

--- hello/hello.go
+++ hello/hello.go
@@ -6,7 +6,18 @@ import (
 	"github.com/JakyriSong/go-learn/stringutil"
 )
 
+func defer_test() int {
+	a := 10
+	defer func() {
+		a++
+		fmt.Println(a)
+	}()
+	fmt.Println(a)
+	return a
+}
+
 func main() {
 	fmt.Printf("Hello, world.\n")
 	fmt.Println(stringutil.Reverse("Hello, Go!"))
+	fmt.Println(defer_test())
 }
--- slice/slice.go
+++ slice/slice.go
@@ -2,29 +2,33 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"strings"
 )
 
-func addElem(b *[]byte) {
-	*b = append(*b, 'a')
+func addElem(b []byte) {
+	//s := []byte{'a', 's', 'd'}
+	fmt.Printf("b: %p\n", &b)
+	//b[0] = 'A'
+	b = append(b, 'c')
+	fmt.Printf("b: %p ", &b)
+	fmt.Println(b)
 	return
 }
 
 func main() {
-	c := make([]byte, 0, 5)
-	addElem(&c)
-	fmt.Printf("%c\n", c[0])
+	c := make([]byte, 1)
+	addElem(c)
+	fmt.Printf("c: %p ", &c)
+	fmt.Println(c)
 
-	r := strings.NewReader("Hello, Reader!")
+	// r := strings.NewReader("Hello, Reader!")
 
-	b := make([]byte, 8)
-	for {
-		n, err := r.Read(b)
-		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
-		fmt.Printf("b[:n] = %q\n", b[:n])
-		if err == io.EOF {
-			break
-		}
-	}
+	// b := make([]byte, 8)
+	// for {
+	// 	n, err := r.Read(b)
+	// 	fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
+	// 	fmt.Printf("b[:n] = %q\n", b[:n])
+	// 	if err == io.EOF {
+	// 		break
+	// 	}
+	// }
 }
